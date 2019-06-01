f1d0a0fb829025fc6e2288b781290fdc25836ca1
Hu Xinmin <huxinmin_dlmuit@163.com>
2019-06-01 13:16:03 +0800

    reminder:second commit

--- 2018-08-14-04-15-45-1534220145-master/main/main.js
+++ 2018-08-14-04-15-45-1534220145-master/main/main.js
@@ -1,8 +1,9 @@
 // Write your cade below:
 module.exports = function main(num1,num2) {
-	var reminder;
+	
 	if(num2!=0){
-		reminder=num1%num2;
+		var reminder=num1%num2;
+		return reminder;
 	}
-	return reminder;
+
 };
